59d3e725ccdbf623fc3b9e3c112a8b80618b6f84
Ninochakvetadze <nino.chakvetadze28b@geolab.edu.ge>
2023-04-28 04:00:08 -0700

    md

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,6 +1,23 @@
+import { PageLink } from "../../globalComponents";
+
 export function Footer() {
+    const links = [
+        { to: "/", name: "Home" },
+        { to: "/about", name: "About" },
+        { to: "/contact", name: "Contact" },
+    ];
     return (
         <footer>
+            <nav>
+                <ul>
+                    {links.map((link, index) => {
+                        <li key={index}>
+                            <PageLink to={link.to} name={link.name} />
+                        </li>;
+                    })}
+                </ul>
+            </nav>
+
             <p>Footer div</p>
         </footer>
     );
